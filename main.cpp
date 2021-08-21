ae81560393703a033bbfb167ce40ce5793361ad2
Mario <48070830+mjcarranza@users.noreply.github.com>
2021-08-21 15:00:51 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,4 +5,9 @@ int main() {
 	// PRUEBAS
 
 	Linea* L1 = new Linea();
+	
+	// en esta parte se llaman los metodos de línea pasando los parametros requeridos por cada funcion, por ejemplo:
+	L1->informacion();	// obtiene las posiciones de los dos puntos de la recta
+	
+	
 }
