ce6221600c1c1bf91288a87c52c3a0997256d5a2
Mario <48070830+mjcarranza@users.noreply.github.com>
2021-08-21 14:51:39 -0600

    Update Linea.cpp

--- Linea.cpp
+++ Linea.cpp
@@ -2,21 +2,22 @@
 #include "Linea.h"
 
 double Linea::mueveDerecha(double _dato) {
-
+     // en este método. se debe utilizar _dato como el numero de unidades que se quiere desplazar la recta en dirección las coordenadas en x,
+     // es decir _dato+A1 y _dato+B1 donde las nuevas coordenadas serán (_dato + A1, A2) y (_dato + B1, B2).
 }
 double Linea::mueveIzquierda(double _dato) {
-
+     // hacer lo mismo que en el m[etodo anterior pero el movimiento va hacia la izquierda
 }
 double Linea::mueveArriba(double _dato) {
-
+     // hacer lo mismo que en el m[etodo anterior pero el movimiento va hacia arriba
 }
 double Linea::mueveAbajo(double _dato) {
-
+     // hacer lo mismo que en el m[etodo anterior pero el movimiento va hacia abajo
 }
 double Linea::informacion() {
-     double recta[2];
-     double puntoA[2];
-     double puntoB[2];
-     Punto* A=A[0]
+     double recta[2];    // array o lista que va a almacenar la lista del punto A y la del punto B.
+     double puntoA[2];   // array o lista que va a almacenar las coordenadas del punto A.
+     double puntoB[2];   // array o lista que va a almacenar las coordenadas del punto A.
+     Punto* A=A[0]       // aquí se debe llamar el metodo get
      
 }
